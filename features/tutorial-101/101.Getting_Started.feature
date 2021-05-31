826db2d615dd42d1e1da60421f66e5d8f124c18f
Amybough <ameniboughanmi96@gmail.com>
2021-05-31 12:40:37 +0000

    first test

--- features/tutorial-101/101.Getting_Started.feature
+++ features/tutorial-101/101.Getting_Started.feature
@@ -1,8 +1,10 @@
 # created by Amani Boughanmi on 20.05.2021
-Feature: Test tutorial 101. Getting Started
 
-  This is the feature file of the FIWARE Step by Step tutorial for NGSI-v2
-  url: https://fiware-tutorials.readthedocs.io/en/latest/getting-started/index.html
-  docker-compose: https://raw.githubusercontent.com/FIWARE/tutorials.Getting-Started/master/docker-compose.yml
-  environment: https://raw.githubusercontent.com/FIWARE/tutorials.Getting-Started/master/.env
+Feature: test tutorial 101.Getting Started
+
 Background: 
+    Given I set the tutorial
+
+Scenario: GET version request
+	Given I send GET HTTP request
+	Then the json response is valid
--- features/tutorial-101/environment.py
+++ features/tutorial-101/environment.py
@@ -19,16 +19,16 @@ def before_feature(context, feature):
     __logger__.info("=========== START FEATURE =========== ")
     __logger__.info("Feature name: %s", feature.name)
 
-    parameters = [s for s in feature.description if 'docker-compose' in s or 'environment' in s]
-    parameters = dict(s.split(':', 1) for s in parameters)
+    #parameters = [s for s in feature.description if 'docker-compose' in s or 'environment' in s]
+    #parameters = dict(s.split(':', 1) for s in parameters)
 
-    r = get(parameters['docker-compose'], allow_redirects=True)
-    open('docker-compose.yml', 'wb').write(r.content)
+    #r = get(parameters['docker-compose'], allow_redirects=True)
+    #open('docker-compose.yml', 'wb').write(r.content)
 
-    r = get(parameters['environment'], allow_redirects=True)
-    open('.env', 'wb').write(r.content)
+    #r = get(parameters['environment'], allow_redirects=True)
+    #open('.env', 'wb').write(r.content)
 
-    docker.compose.up(detach=True)
+    #docker.compose.up(detach=True)
 
 
 def before_scenario(context, scenario):
@@ -42,11 +42,11 @@ def after_scenario(context, scenario):
 
 def after_feature(context, feature):
     __logger__.info("=========== END FEATURE =========== ")
-    docker.compose.down()
+    #docker.compose.down()
 
-    files = ['docker-compose.yml', '.env']
+    #files = ['docker-compose.yml', '.env']
 
-    [remove(f) for f in files if exists(f)]
+    #[remove(f) for f in files if exists(f)]
 
 
 def after_all(context):
