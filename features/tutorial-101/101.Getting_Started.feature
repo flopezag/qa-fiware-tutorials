d1c2de64953ed889620e386ab8a544f1c38e4d3b
Amybough <ameniboughanmi96@gmail.com>
2021-06-21 13:18:35 +0000

    Post scenarios added

--- features/tutorial-101/101.Getting_Started.feature
+++ features/tutorial-101/101.Getting_Started.feature
@@ -1,15 +1,70 @@
-# created by Amani Boughanmi on 20.05.2021
-
 Feature: test tutorial 101.Getting Started
 
- This is the feature file of the FIWARE Step by Step tutorial for NGSI-v2
- url: https://fiware-tutorials.readthedocs.io/en/latest/getting-started/index.html
- docker-compose: https://raw.githubusercontent.com/FIWARE/tutorials.Getting-Started/master/docker-compose.yml
- environment: https://raw.githubusercontent.com/FIWARE/tutorials.Getting-Started/master/.env
+  This is the feature file of the FIWARE Step by Step tutorial for NGSI-v2
+  url: https://fiware-tutorials.readthedocs.io/en/latest/getting-started/index.html
+  docker-compose: https://raw.githubusercontent.com/FIWARE/tutorials.Getting-Started/master/docker-compose.yml
+  environment: https://raw.githubusercontent.com/FIWARE/tutorials.Getting-Started/master/.env
 
 Background: 
     Given I set the tutorial
 
 Scenario: GET version request
-	Given I send GET HTTP request
-	Then the json response is valid
+    When  I send GET HTTP request to "http://localhost:1026/version"
+    Then  I receive a HTTP "200" response code
+    And   I receive a dictionary with the key "orion" and the following data
+          | version     | uptime       | git_hash                                 | compile_time                |
+          | 3.0.0       |              | d6f8f4c6c766a9093527027f0a4b3f906e7f04c4 | Mon Apr 12 14:48:44 UTC 2021|
+    And   also the following data
+          | compiled_by | compiled_in  | release_date                | doc                                     |
+          | root        | f307ca0746f5 | Mon Apr 12 14:48:44 UTC 2021| "https://fiware-orion.rtfd.io/en/3.0.0/ |
+    And   there is no other information on it
+
+
+Scenario: POST request2  
+    When I set the following request body to "request001.json"
+    And  I send POST HTTP request to "http://localhost:1026/v2/entities"
+    Then I receive a HTTP "201" code response
+    And  I receive this dictionary
+         | Connection | Content-Length| Location                                     |          
+         | Keep-Alive | 0             | /v2/entities/urn:ngsi-ld:Store:001?type=Store|
+    And also the following keys
+         | Fiware-correlator | Date |
+
+Scenario: POST request3 
+    When I set the following request body to "Tests/features/tutorial-101/steps/request02.json"
+    And  I send POST HTTP request to "http://localhost:1026/v2/entities"
+    Then I receive a HTTP "201" code response
+    And  I receive this dictionary
+         | Connection | Content-Length| Location                                     |          
+         | Keep-Alive | 0             | /v2/entities/urn:ngsi-ld:Store:002?type=Store|
+    And also the following variables
+         | Fiware-correlator | Date |
+
+Scenario: GET request4
+    When I send GET HTTP request to "http://localhost:1026/v2/entities/urn:ngsi-ld:Store:001"
+    Then I receive a HTTP "200" code response
+    And  I receive the dictionary below
+         | id                    | type  | address                                                  | location         | name              |
+         | urn:ngsi-ld:Store:001 | store | streetAddress, addressRegion, addressLocality, postalCode| type, coordinates| Bösebrücke Einkauf|
+    And  I receive the following info with the "address" key
+         | streetAdress         | addressRegion | addressLocality | postalCode |
+         | Bornholmer Straße 65 | Berlin        | Prenzlauer Berg | 10439      |
+
+    And  I receive the following info with the "location" key
+         | type  | coordinates        |
+         | Point | 13.3986, 52.5547   |
+
+
+Scenario: GET request5
+    When I send GET HTTP request to "http://localhost:1026/v2/entities"
+    Then I receive a HTTP "200" code response
+    And  I receive the entities dictionary 
+
+
+
+
+
+    
+
+
+
--- features/tutorial-101/steps/101.Getting_Started.py
+++ features/tutorial-101/steps/101.Getting_Started.py
@@ -1,80 +1,152 @@
 # created by Amani Boughanmi on 20.05.2021
 
-from behave import *
-import requests
+from behave import given, when, then, step
+from requests import get, exceptions
+from hamcrest import assert_that, is_, has_key
+import os
 import json
-import shlex
-import subprocess
-#from json_schema.json_differ import diff_jsons
-global actual_response
-
-expected_response = (["""
-  {
-  "orion": {
-    "version": "3.0.0",
-    "uptime": "0 d, 0 h, 17 m, 19 s",
-    "git_hash": "d6f8f4c6c766a9093527027f0a4b3f906e7f04c4",
-    "compile_time": "Mon Apr 12 14:48:44 UTC 2021",
-    "compiled_by": "root",
-    "compiled_in": "f307ca0746f5",
-    "release_date": "Mon Apr 12 14:48:44 UTC 2021",
-    "machine": "x86_64",
-    "doc": "https://fiware-orion.rtfd.io/en/3.0.0/"
-    "libversions": {
-     "boost": "1_66",
-     "libcurl": "libcurl/7.61.1 OpenSSL/1.1.1g zlib/1.2.11 nghttp2/1.33.0",
-     "libmicrohttpd": "0.9.70",
-     "openssl": "1.1",
-     "rapidjson": "1.1.0",
-     "mongoc": "1.17.4",
-     "bson": "1.17.4"
-    }
-  }
- }
-"""])
-
-class AttrDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
 
 
 @given(u'I set the tutorial')
 def step_impl(context):
-	assert True
+    # Nothing to do so far, at the moment
+    pass
 
-# START GET/version Scenario
-@given(u'I Send GET HTTP request')
-def step_impl(context):
-    global actual_response
-    cUrl= r""" curl -X GET http://localhost:1026/version """
-    # split the cUrl into an array
-    sp_cUrl = shlex.split(cUrl)
-    process = subprocess.Popen(sp_cUrl, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-   # acquire the response and the err if exists
-    resp, err = process.communicate() 
-    # transform json response to python dict
-    actual_response = json.loads(str(resp.decode("UTF-8"))) 
-    print (actual_response)
 
-@then(u'the json response is valid')
-def step_impl(context):
-    assert True
+@when(u'I send GET HTTP request to "{url}"')
+def send_orion_get_version(context, url):
+    try:
+        response = get(url)
+    except exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)
 
-# END GET/version Scenario
+    context.response = response.json()
+    context.statusCode = str(response.status_code)
 
-    #raise NotImplementedError(u'Then the json response is valid')
 
-    
+@step(u'I receive a HTTP "{status_code}" response code')
+def http_code_is_returned(context, status_code):
+    assert_that(context.statusCode, is_(status_code),
+                "Response to CB notification has not got the expected HTTP response code: Message: {}"
+                .format(context.response))
 
-    """if (actual_response['orion']['version'] == expected_response['orion']['version']):
-        assert True
-    if (actual_response[0][0] == expected_response[0][0]): 
-        assert True"""
-     
-    
-    
-    
+
+@then(u'I receive a dictionary with the key "{key}" and the following data')
+def receive_orion_version_response1(context, key):
+    assert_that(context.response, has_key(key),
+                "Response to CB notification has not got the 'orion' key: Message: {}"
+                .format(context.response))
+    value = context.response[key]
+    for element in context.table.rows:
+        expected_message = dict(element.as_dict())
+        print(expected_message)
+        for key_version in expected_message.keys():
+            assert_that(value, has_key(key_version))
+            assert_that(value[key_version], is_(expected_message[key_version]),
+                        "The value of key {} received is: {}, it is not the expected one: {}"
+                        .format(key, value[key_version], expected_message[key_version]))
+
+@then(u'also the following data')
+def check_dict_content_request_version2(context):
+    raise NotImplementedError(u'STEP: also the following data')
+
+
+@then(u'there is no other information on it')
+def check_dict_content_request_version2(context):
+    raise NotImplementedError(u'STEP: there is no other information on it')
+
+# POST request2
+
+@When(u'I set the following request body {body}')
+def set_req_body(context, body):
+    path = r'/home/ubuntu/'
+    for body in os.listdir(path):
+        with open( body, 'r') as json_file:
+            data = json_file.read()
+            print(data)
+            json_data = json.loads(data)
+        
+@step(u'I send POST HTTP request to "{url}"')
+def send_orion_post_entity1(context, url):
+    headers = {'Content-Type' : 'application/json'}
+    try:
+        response = post(url, data = json_data, headers = headers)
+    except exceptions.RequestException as e:  
+        raise SystemExit(e)
+
+    context.response = response.json()
+    context.statusCode = str(response.status_code)
+
+@then (u'I receive a HTTP {status_code} code response')
+def http_post_code_returned(context, status_code):
+    assert_that(context.statusCode, is_(status_code),
+                "Response to CB notification has not got the expected HTTP response code: Message: {}"
+                .format(context.response))
+
+@then (u'I receive this dictionary')
+def receive_post_response1(context):
+
+    for element in context.table.rows:
+        valid_response = dict(element.as_dict())
+        print(valid_response)
+        for key in valid_response.keys():
+            assert_that(context.response, has_key(key))
+            assert_that(context.response[key], is_(valid_response[key]),
+                        "The value of key {} received is: {}, it is not the expected one: {}"
+                        .format(key, context.response[key], valid_response[key]))
+
+@then (u'also the following keys')
+def check_dict_post_request(context):
+    for key in valid_response.keys():
+        assert_that(context.response, has_key(key),
+                    "The key {} received is not the expected one:"
+                    .format(key))
+
+# POST request3
+@When(u'I set the following request body {body}')
+def set_req_body(context, body):
+    path = r'/home/ubuntu/'
+    for body in os.listdir(path):
+        with open( body, 'r') as json_file:
+            data = json_file.read()
+            print(data)
+            json_data = json.loads(data)
+        
+@step(u'I send POST HTTP request to "{url}"')
+def send_orion_post_entity1(context, url):
+    headers = {'Content-Type' : 'application/json'}
+    try:
+        response = post(url, data = json_data, headers = headers)
+    except exceptions.RequestException as e:  
+        raise SystemExit(e)
+
+    context.response = response.json()
+    context.statusCode = str(response.status_code)
+
+@then (u'I receive a HTTP {status_code} code response')
+def http_post_code_returned(context, status_code):
+    assert_that(context.statusCode, is_(status_code),
+                "Response to CB notification has not got the expected HTTP response code: Message: {}"
+                .format(context.response))
+
+@then (u'I receive this dictionary')
+def receive_post_response1(context):
+
+    for element in context.table.rows:
+        valid_response = dict(element.as_dict())
+        print(valid_response)
+        for key in valid_response.keys():
+            assert_that(context.response, has_key(key))
+            assert_that(context.response[key], is_(valid_response[key]),
+                        "The value of key {} received is: {}, it is not the expected one: {}"
+                        .format(key, context.response[key], valid_response[key]))
+
+@then (u'also the following keys')
+def check_dict_post_request(context):
+    for key in valid_response.keys():
+        assert_that(context.response, has_key(key),
+                    "The key {} received is not the expected one:"
+                    .format(key))
+
 
 	
