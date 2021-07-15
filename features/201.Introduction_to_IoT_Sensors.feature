e3ecfc2323f213033acc1bad3670d4fd277c26f9
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2021-07-15 11:11:47 +0200

    Feature 201 - IoT Sensors implemented and changed environment.py

---
+++ features/201.Introduction_to_IoT_Sensors.feature
@@ -0,0 +1,31 @@
+Feature: test tutorial 201.Introductio to IoT Sensors
+
+  This is the feature file of the FIWARE Step by Step tutorial for IoT Sensors - What's Ultralight 2.0
+  url: https://fiware-tutorials.readthedocs.io/en/latest/iot-sensors.html
+  environment: https://raw.githubusercontent.com/FIWARE/tutorials.IoT-Sensors/NGSI-v2/.env
+  git-clone: https://github.com/FIWARE/tutorials.IoT-Sensors.git
+  git-directory: /tmp/tutorials.IoT-Sensors
+  shell-commands: ./services create; ./services start
+  clean-shell-commands: ./services stop
+  NOTES: request201-08.txt will faill. Should be corrected in tutorial.
+
+
+  Background:
+    Given I set the tutorial 201
+
+
+    Scenario Outline: Communicating with IoT Devices: Using Actuators
+    When  I send POST HTTP IoT request for "<description>" to "http://localhost:3001"/"<location>"
+    And   With the body IoT request described in "<request_file>"
+    Then  I receive a HTTP "200" IoT response code with the body "<response_file>"
+
+    Examples:
+        | request_file           | location    | response_file      | description     |
+        | request201-01.txt      | iot/bell001 | response201-01.txt | ring bell       |
+        | request201-02.txt      | iot/lamp001 | response201-02.txt | Switch lamp on  |
+        | request201-03.txt      | iot/lamp001 | response201-03.txt | Switch lamp off |
+        | request201-04.txt      | iot/lamp001 | response201-04.txt | Switch lamp on  |
+        | request201-05.txt      | iot/door001 | response201-05.txt | Unlock door     |
+        | request201-06.txt      | iot/door001 | response201-06.txt | Open door       |
+        | request201-07.txt      | iot/door001 | response201-07.txt | Close door      |
+        | request201-08.txt      | iot/door001 | response201-08.txt | Lock door       |
--- features/environment.py
+++ features/environment.py
@@ -8,8 +8,41 @@ from requests import get
 from os.path import exists
 from os import remove
 from sys import stdout
+import subprocess
+import os
+from shutil import rmtree
 
 __logger__ = getLogger(__name__)
+
+INTERESTING_FEATURES_STRINGS = ['docker-compose', 'environment', 'git-clone', 'shell-commands', 'git-directory']
+
+
+def is_interesting_feature_string(feature_description: str):
+    for f in INTERESTING_FEATURES_STRINGS:
+        if f in feature_description:
+            return True
+    return False
+
+
+def git(*args):
+    try:
+        subprocess.check_output(['/usr/bin/git'] + list(args), stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        stdout.write(f'\n -- GIT EXCEPTION-- \n\n{e.output}\n')
+        __logger__.error("Exception on process, rc=", e.returncode, "output=", e.output)
+
+
+def exec_commands(parameters:dict, which_commands:str):
+    commands = parameters[which_commands].split(';')
+    commands_dir = parameters['git-directory'] if 'git-directory' in parameters else '.'
+
+    current_dir = os.getcwd()
+    os.chdir(commands_dir)
+
+    for command in commands:
+        os.system(command.strip())
+
+    os.chdir(current_dir)
 
 
 def before_all(context):
@@ -24,16 +57,29 @@ def before_feature(context, feature):
     stdout.write("=========== START FEATURE ===========\n")
     stdout.write(f'Feature name: {feature.name}\n\n')
 
-    parameters = [s for s in feature.description if 'docker-compose' in s or 'environment' in s]
-    parameters = dict(s.split(':', 1) for s in parameters)
+    p = [s for s in feature.description if is_interesting_feature_string(s)]
+    parameters = {}
+    # parameters = dict(s.split(':', 1) for s in parameters)
+    context.parameters = parameters
+    for k,v in (s.split(':', 1) for s in p):
+        parameters[k.strip()] = v.strip()
 
-    r = get(parameters['docker-compose'], allow_redirects=True)
-    open('docker-compose.yml', 'wb').write(r.content)
+    if 'docker-compose' in parameters:
+        r = get(parameters['docker-compose'], allow_redirects=True)
+        open('docker-compose.yml', 'wb').write(r.content)
 
-    r = get(parameters['environment'], allow_redirects=True)
-    open('.env', 'wb').write(r.content)
+    if 'environment' in parameters:
+        r = get(parameters['environment'], allow_redirects=True)
+        open('.env', 'wb').write(r.content)
 
-    docker.compose.up(detach=True)
+    if 'docker-compose' in parameters:
+        docker.compose.up(detach=True)
+
+    if 'git-clone' in parameters:
+        git("clone", parameters['git-clone'], parameters['git-directory'])
+
+    if 'shell-commands' in parameters:
+        exec_commands(parameters, 'shell-commands')
 
 
 def before_scenario(context, scenario):
@@ -56,13 +102,18 @@ def after_scenario(context, scenario):
 
 def after_feature(context, feature):
     __logger__.info("=========== END FEATURE ===========")
-    stdout.write(f'=========== END FEATURE ===========\n')
+    stdout.write(f'\n=========== END FEATURE ===========\n')
 
-    docker.compose.down()
+    if 'clean-shell-commands' in context.parameters:
+        exec_commands(context.parameters, 'clean-shell-commands')
 
-    files = ['docker-compose.yml', '.env']
+    if 'docker-compose' in context.parameters:
+        docker.compose.down()
+        files = ['docker-compose.yml', '.env']
+        [remove(f) for f in files if exists(f)]
 
-    [remove(f) for f in files if exists(f)]
+    if 'git-directory' in context.parameters:
+        rmtree(context.parameters['git-directory'])
 
 
 def after_all(context):
