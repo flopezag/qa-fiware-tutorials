013e0b95055a381ab94ba9aaca9519dda953c8a8
Fernando Lopez <fernando.lopez@fiware.org>
2022-01-25 12:13:36 +0100

    Assigning permissions to each role operations

--- features/steps/402.Managing_roles_and_permissions.py
+++ features/steps/402.Managing_roles_and_permissions.py
@@ -1,4 +1,4 @@
-from behave import given, when, step
+from behave import given, when, step, then
 from config import settings
 from os.path import join
 
@@ -50,3 +50,49 @@ def step_impl(context):
     :type context: behave.runner.Context
     """
     context.url = f'http://localhost:3005/v1/applications/{settings.applicationId}/roles/{settings.roleId}'
+
+
+@step("I set the permission to the role of an application")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    context.url = f'http://localhost:3005/v1/applications/{settings.applicationId}/roles/{settings.roleId}' \
+                  f'/permissions/{settings.permissionId}'
+
+
+@step("I set the permissions url to the role of an application")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    context.url = f'http://localhost:3005/v1/applications/{settings.applicationId}/roles/{settings.roleId}/permissions'
+
+
+@then(
+    'I receive a HTTP "{code}" status code from Keyrock with the "role_permission_assignments" with this'
+    ' "roleId" and "permissionId"'
+)
+def step_impl(context, code):
+    """
+    :type context: behave.runner.Context
+    """
+    assert (context.statusCode == code), \
+        f'The status code is not the expected value, received {context.statusCode}, expected {code}'
+
+    assert ("role_permission_assignments" in context.response), \
+        f'The Response of the Keyrock does not contain the key "role_permission_assignments"'
+
+    assert ("role_id" in context.response['role_permission_assignments']), \
+        f'The Response of the Keyrock does not contain the subkey "role_id"'
+
+    assert ("permission_id" in context.response['role_permission_assignments']), \
+        f'The Response of the Keyrock does not contain the subkey "permission_id"'
+
+    assert (context.response['role_permission_assignments']['role_id']), \
+        f"The role_id received is not the expected value, received: " \
+        f"{context.response['role_permission_assignments']['role_id']}, but was expected {settings.roleId}"
+
+    assert (context.response['role_permission_assignments']['permission_id']), \
+        f"The permission_id received is not the expected value, received: " \
+        f"{context.response['role_permission_assignments']['permission_id']}, but was expected {settings.permissionId}"
