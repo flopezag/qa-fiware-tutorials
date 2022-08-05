9c9ec75d1b2281ec56f248387c86f180a479b465
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2022-08-05 11:59:15 +0200

    Removed the debug part and prepared for PR

--- features/steps/302.ngsild.BigDataFlink.py
+++ features/steps/302.ngsild.BigDataFlink.py
@@ -6,7 +6,3 @@ from time import sleep
 @given(u'I set the tutorial 302 LD - Big Data Flink')
 def step_impl(context):
     context.data_home = join(join(join(CODE_HOME, "features"), "data"), "302.ld.BigData_Flink")
-
-@step(u'I wait for debug')
-def wait_for_debug_302ld(context):
-   sleep(1)
